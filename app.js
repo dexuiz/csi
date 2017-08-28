d43fa39257c531206d5d7bfe055dae5292a230ff
dexuiz <dex345z@gmail.com>
2017-08-28 17:23:46 +0530

    port  binded

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var express = require("express");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended:true}));
 
 mongoose.connect("mongodb://dexuiz:deval@ds151433.mlab.com:51433/csi");
 
-app.listen(3000||process.env.PORT,function(){
+app.listen(port,function(){
   console.log("csi server on 3000");
 })
